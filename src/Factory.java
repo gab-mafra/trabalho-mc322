71f6b701c8ca2e374fc4e157de1805e48ef7630b
Victor Toon <araujo.victor200@gmail.com>
2019-06-07 20:05:17 -0300

    Criei a Factory

---
+++ src/Factory.java
@@ -0,0 +1,18 @@
+public class Factory {
+    public protocoloUrgencia criarUrgencia(){
+        return new protocoloUrgencia();
+    }
+
+    public Grafico criarGrafico(Patient[] pacientes){
+        return new Grafico(pacientes);
+    }
+
+    public Prontuario criairProntuario(){
+        return new Prontuario();
+    }
+
+    public MergeSort criarMergeSort(){
+        return new MergeSort();
+    }
+
+}
